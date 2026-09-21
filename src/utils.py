4f63cbed049d268c80c5d95c2670c69fcf496a22
elopezarb <106501475+elopezarb@users.noreply.github.com>
2026-09-20 21:38:06 -0400

    added Svensson formula and model structure

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 # libraries
 import numpy as np
 import pandas as pd
+from pandas.tseries.offsets import MonthEnd
 
 
 
@@ -24,20 +25,77 @@ def treasury_form_2_decimal(x):
     decimal_price = units + cents / 32 + thousandths / 256
     return decimal_price
 
+
+
+
+def previous_coupon_date(date, preserve_month_end):
+    """
+    Get the previous coupon date for a given date.
+    If the maturity date is a month end, then the previous coupon date should also be a month end.
+    """ 
+
+    previous = pd.Timestamp(date) - pd.DateOffset(months=6)
+
+    if preserve_month_end:
+        previous = previous + MonthEnd(0)
+
+    return previous
+
+
+def coupon_schedule(maturity, valuation_date):
+    """
+    Get the coupon schedule for a given maturity and valuation date.
+
+    Parameters:
+    maturity (str): The maturity date of the bond in 'YYYY-MM-DD' format.
+    valuation_date (str): The valuation date in 'YYYY-MM-DD' format.
+    """
+    maturity = pd.Timestamp(maturity).normalize()
+    valuation_date = pd.Timestamp(valuation_date).normalize()
+    preserve_month_end = maturity.is_month_end
+
+    dates = []
+    coupon_date = maturity
+
+    while coupon_date > valuation_date:
+        dates.append(coupon_date)
+        coupon_date = previous_coupon_date(
+            coupon_date,
+            preserve_month_end,
+        )
+
+    return sorted(dates)
+
 def current_coupon_dates(maturity, valuation_date):
-    maturity = pd.Timestamp(maturity)
-    valuation_date = pd.Timestamp(valuation_date)
-    six_months = pd.DateOffset(months=6)
+    """
+    Get the current coupon dates for a given maturity and valuation date.
 
-    coupon_end = maturity
+    Parameters:
+    maturity (str): The maturity date of the bond in 'YYYY-MM-DD' format.
+    valuation_date (str): The valuation date in 'YYYY-MM-DD'
+    
+    """
+    maturity = pd.Timestamp(maturity).normalize()
+    valuation_date = pd.Timestamp(valuation_date).normalize()
+    preserve_month_end = maturity.is_month_end
 
-    # Move backward until the next coupon is after valuation_date
-    while coupon_end - six_months > valuation_date:
-        coupon_end -= six_months
+    next_coupon = maturity
 
-    coupon_start = coupon_end - six_months
+    while previous_coupon_date(
+        next_coupon,
+        preserve_month_end,
+    ) > valuation_date:
+        next_coupon = previous_coupon_date(
+            next_coupon,
+            preserve_month_end,
+        )
+
+    previous_coupon = previous_coupon_date(
+        next_coupon,
+        preserve_month_end,
+    )
 
     return pd.Series({
-        "Accrual Start": coupon_start,
-        "Accrual End": coupon_end,
+        "Accrual Start": previous_coupon,
+        "Accrual End": next_coupon,
     })
